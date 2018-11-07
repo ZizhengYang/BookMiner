6a121e35a268596a6be5a1067014fcdc95699b8a
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2018-11-06 20:49:27 -0600

    Add files via upload

---
+++ UI/HomePage/HomePage.js
@@ -0,0 +1,17 @@
+function fadd(){
+
+    window.open("./addDialog/addDialog.html", "hahah", "width=600,height=400,top=200,left=400,menubar=no,toolbar=no,status=no,scrollbars=yes,location=no,resizable=no");
+
+}
+
+function fdelete(){
+
+    window.open("./deleteDialog/deleteDialog.html", "hahah", "width=600,height=400,top=200,left=400,menubar=no,toolbar=no,status=no,scrollbars=yes,location=no,resizable=no");
+
+}
+
+function fshare() {
+
+    window.open("./shareDialog/shareDialog.html", "hahah", "width=600,height=400,top=200,left=400,menubar=no,toolbar=no,status=no,scrollbars=yes,location=no,resizable=no");
+
+}
