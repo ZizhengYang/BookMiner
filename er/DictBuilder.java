7b761fa61dc49d989cfeeede084bbc47a54b189c
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2018-11-08 21:36:51 -0600

    Add files via upload

---
+++ er/DictBuilder.java
@@ -0,0 +1,28 @@
+package er;
+
+import java.util.ArrayList;
+
+/**
+ * This class help to build a dict tree for the analyze.
+ * You can input the following types of variables to the constructor:
+ * <strong> String, </strong>
+ * <strong>character array, </strong>
+ * <strong>String array, </strong>
+ * <strong>File object, </strong>
+ * <strong></strong>
+ */
+public class DictBuilder {
+
+    DictBuilder(String str){
+
+
+
+    }
+
+    DictBuilder(char[] arr){
+
+
+
+    }
+
+}
